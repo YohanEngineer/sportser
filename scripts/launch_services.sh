de303a38b58cb49cfc4b7af2d333fb30d5ae9a59
YohanEngineer <bouali.yohan@yahoo.com>
2023-04-01 19:01:18 +0200

    Add forgotten script and remove dependency

---
+++ scripts/launch_services.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+osascript -e 'tell application "Terminal" to do script "docker-compose -f /Users/yohan/IdeaProjects/sportser/mongo-configuration/mongo.yaml up"'
+osascript -e 'tell application "Terminal" to do script "docker-compose -f /Users/yohan/IdeaProjects/sportser/mysql-configuration/mysql.yaml up"'
+osascript -e 'tell application "Terminal" to do script "docker-compose -f /Users/yohan/IdeaProjects/sportser/redis-configuration/redis.yaml up"'
+osascript -e 'tell application "Terminal" to do script "/opt/homebrew/opt/mosquitto/sbin/mosquitto -c /opt/homebrew/etc/mosquitto/mosquitto.conf"'
+osascript -e 'tell application "Terminal" to do script "cd /Users/yohan/Downloads/kafka_2.13-3.4.0/bin/ && ./zookeeper-server-start.sh ../config/zookeeper.properties"'
+osascript -e 'tell application "Terminal" to do script "cd /Users/yohan/Downloads/kafka_2.13-3.4.0/bin/ && ./kafka-server-start.sh ../config/server.properties"'
+
