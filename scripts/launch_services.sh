5d7aa217f29262f7b61de3d4be3af1b6f5c0704e
YohanEngineer <bouali.yohan@yahoo.com>
2023-04-02 16:43:17 +0200

    Add kafka source files

--- scripts/launch_services.sh
+++ scripts/launch_services.sh
@@ -6,4 +6,4 @@ osascript -e 'tell application "Terminal" to do script "docker-compose -f /Users
 osascript -e 'tell application "Terminal" to do script "/opt/homebrew/opt/mosquitto/sbin/mosquitto -c /opt/homebrew/etc/mosquitto/mosquitto.conf"'
 osascript -e 'tell application "Terminal" to do script "cd /Users/yohan/Downloads/kafka_2.13-3.4.0/bin/ && ./zookeeper-server-start.sh ../config/zookeeper.properties"'
 osascript -e 'tell application "Terminal" to do script "cd /Users/yohan/Downloads/kafka_2.13-3.4.0/bin/ && ./kafka-server-start.sh ../config/server.properties"'
-
+osascript -e 'tell application "Terminal" to do script "cd /Users/yohan/Downloads/kafka_2.13-3.4.0/bin/ && ./connect-standalone.sh ../config/connect-standalone.properties ../config/mongodb-sink.properties"'
